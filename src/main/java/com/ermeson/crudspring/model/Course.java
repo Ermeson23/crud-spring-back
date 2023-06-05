a7c5b864e8ba1c149abfe631a6af1e152d8aeb30
Ermeson23 <joseenem35@gmail.com>
2023-06-05 15:05:40 -0300

    applying logical removal(soft delete) from a course

--- src/main/java/com/ermeson/crudspring/model/Course.java
+++ src/main/java/com/ermeson/crudspring/model/Course.java
@@ -1,21 +1,22 @@
 package com.ermeson.crudspring.model;
 
-import org.hibernate.validator.constraints.Length;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 //@Table(name =  "cursos") - caso a tabela receba um nome diferente do Banco de Dados
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?" )
+@Where(clause = "status = 'Ativo'")
 public class Course {
     
     @Id
@@ -35,6 +36,12 @@ public class Course {
     @Pattern(regexp = "Front-end|Back-end|Full-stack")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 
     public Long getId() {
         return id;
