93a4acf0d02dc36b7096da4fbb942089c035504a
Ermeson23 <joseenem35@gmail.com>
2023-06-27 16:51:58 -0300

    applying the DTO and Mapper patterns

--- src/main/java/com/ermeson/crudspring/controller/CourseController.java
+++ src/main/java/com/ermeson/crudspring/controller/CourseController.java
@@ -1,6 +1,6 @@
 package com.ermeson.crudspring.controller;
 
-import com.ermeson.crudspring.model.Course;
+import com.ermeson.crudspring.dto.CourseDTO;
 import com.ermeson.crudspring.service.CourseService;
 
 import jakarta.validation.Valid;
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Positive;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,25 +35,25 @@ public class CourseController {
 
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public @ResponseBody List<Course> list() {
+    public @ResponseBody List<CourseDTO> list() {
         return courseService.list();
     }
 
     @GetMapping("/{id}")
-    public Course findById(@PathVariable @NotNull @Positive Long id) {
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
         return courseService.findById(id);
     }
 
     //@RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Course create(@RequestBody @Valid Course course) {
+    public CourseDTO create(@RequestBody @Valid CourseDTO course) {
        return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public Course update(@PathVariable @NotNull @Positive Long id,
-        @RequestBody @Valid Course course) {
+    public CourseDTO update(@PathVariable @NotNull @Positive Long id,
+        @RequestBody @Valid @NotNull CourseDTO course) {
         return courseService.update(id, course);
     }
 
